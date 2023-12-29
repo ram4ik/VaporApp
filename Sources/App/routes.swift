f05857c6bbf4ac4eb4327e128820967cf71c889f
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-29 15:34:13 +0200

    VaporApp init

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,38 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("restaurants", "speciality", "chinese") { req async -> String in
+        "restaurants/speciality/chinese"
+    }
+    
+    app.get("restaurants", "speciality", "indian") { req async -> String in
+        "restaurants/speciality/indian"
+    }
+    
+    app.get("restaurants", "speciality", "thai") { req async -> String in
+        "restaurants/speciality/thai"
+    }
+    
+    app.get("restaurants", "speciality", ":region") { req -> String in
+        guard let region = req.parameters.get("region") else {
+            throw Abort(.badRequest)
+        }
+        return "restaurants/speciality/\(region)"
+    }
+    
+    app.get("restaurants", ":location", "speciality", ":region") { req -> String in
+        guard let locaton = req.parameters.get("location"), let region = req.parameters.get("region") else {
+            throw Abort(.badRequest)
+        }
+        return "restaurants in \(locaton) with speciality \(region)"
+    }
+    
+    app.get("routeany", "*", "endpoint") { req -> String in
+        return "This is anything route"
+    }
+    
+    app.get("routeany", "**") { req -> String in
+        return "This is Catch All route"
+    }
 }
--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -12,4 +12,92 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
+    
+    func testRestaurantsSpecialityChinese() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "/restaurants/speciality/chinese", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "restaurants/speciality/chinese")
+        })
+    }
+    
+    func testRestaurantsSpecialityIndian() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "/restaurants/speciality/indian", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "restaurants/speciality/indian")
+        })
+    }
+    
+    func testRestaurantsSpecialityThai() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "/restaurants/speciality/thai", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "restaurants/speciality/thai")
+        })
+    }
+    
+    func testRestaurantsSpecialityEstonia() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "/restaurants/speciality/estonia", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "restaurants/speciality/estonia")
+        })
+    }
+    
+    func testRestaurantsTallinnSpecialityEstonia() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "restaurants/tallinn/speciality/estonia", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "restaurants in tallinn with speciality estonia")
+        })
+    }
+
+    func testRouteanyTallinnEndpoint() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "/routeany/tallinn/endpoint", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "This is anything route")
+        })
+    }
+    
+    func testRouteanyAll() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "/routeany/all", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "This is Catch All route")
+        })
+    }
+    
+    func testRouteanyAllOther() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "/routeany/xyz/bar/foo", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "This is Catch All route")
+        })
+    }
 }
